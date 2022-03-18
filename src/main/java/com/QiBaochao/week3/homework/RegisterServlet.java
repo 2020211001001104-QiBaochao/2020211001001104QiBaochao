bbd78f204827935593d51a75523c05bf167cbab7
QiBaochao2020211001001104 <2629085134@qq.com>
2022-03-18 20:26:58 +0800

    Week 3 Commit

--- src/main/java/com/QiBaochao/week3/homework/RegisterServlet.java
+++ src/main/java/com/QiBaochao/week3/homework/RegisterServlet.java
@@ -1,77 +1,32 @@
 package com.QiBaochao.week3.homework;
+
 import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
-@WebServlet (name = "RegisterServlet", value = "/register")
 
-
+@WebServlet(name = "RegisterServlet", value = "/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
-    Connection con =null;
     @Override
-    public void init() throws ServletException {
-        super.init();
-        /*
-        String driver = getServletConfig().getServletContext().getInitParameter("driver");
-        String url = getServletConfig().getServletContext().getInitParameter("url");
-        String username = getServletConfig().getServletContext().getInitParameter("username");
-        String password = getServletConfig().getServletContext().getInitParameter("password");
-        try{
-            Class.forName(driver);
-            Connection connection = DriverManager.getConnection(url,username,password);
-            System.out.println("init()" + connection);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }*/
-        con = (Connection) getServletContext().getAttribute("con");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("src/main/webapp/week2/register.jsp").forward(request,response);
-
-    }
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String email = request.getParameter("email");
         String gender = request.getParameter("gender");
         String birthDate = request.getParameter("birthDate");
 
-        try {
-            Statement st = con.createStatement();
-            String sql = "insert into usertable(username,password,email,gender,birthdate)" +
-                    "values('" + username + "','" + password + "','" + email + "','" + gender + "','" + birthDate +"')";
-            System.out.println("sql " + sql);
-
-            int n = st.executeUpdate(sql);
-            System.out.println("n-->" + n);
-
-            sql = "select * from usertable";
-            ResultSet rs = st.executeQuery(sql);
-            PrintWriter out = response.getWriter();
-
-            //request.setAttribute("rsname",rs);
-            //request.getRequestDispatcher("userList.jsp").forward(request,response);
-
-            response.sendRedirect("login");
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-    @Override
-    public void destroy(){
-        super.destroy();
-        try{
-            con.close();
-        }catch (SQLException throwables){
-            throwables.printStackTrace();
-        }
+        PrintWriter writer = response.getWriter();
+        writer.println("<br>username :"+username);
+        writer.println("<br>password :"+password);
+        writer.println("<br>email :"+email);
+        writer.println("<br>gender :"+gender);
+        writer.println("<br>birth Date :"+birthDate);
+        writer.close();
     }
 }
