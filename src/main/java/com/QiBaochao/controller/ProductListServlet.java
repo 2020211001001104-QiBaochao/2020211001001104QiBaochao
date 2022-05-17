65c0927eb0aa108d7c640e257440c7c950f63edf
QiBaochao2020211001001104 <2629085134@qq.com>
2022-05-17 21:06:48 +0800

    Week 12 Commit

--- src/main/java/com/QiBaochao/controller/ProductListServlet.java
+++ src/main/java/com/QiBaochao/controller/ProductListServlet.java
@@ -1,20 +1,30 @@
 package com.QiBaochao.controller;
 
+import com.QiBaochao.dao.ProductDao;
+import com.QiBaochao.model.Product;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/productList")
+@WebServlet(name = "ProductListServlet",value = "/admin/productList")
 public class ProductListServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path="/WEB-INF/views/admin/productList.jsp";
-        request.getRequestDispatcher(path).forward(request,response);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            ProductDao productDao=new ProductDao();
+            List<Product> productList=productDao.findAll((Connection)getServletContext().getAttribute("con"));
+            System.out.println(productList);
+            request.setAttribute("productList",productList);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        String path="/WEB-INF/views/admin/productList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
     }
 }
